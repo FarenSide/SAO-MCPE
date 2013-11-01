3706320bb6fe0989e9d7bc9689b2edb5d426b50d
junyi00 <gohjunyi00@hotmail.com>
2013-11-01 18:17:13 +0800

    Competed Healing

--- SAO-MCPE.php
+++ SAO-MCPE.php
@@ -22,18 +22,25 @@ class SAOMCPE implements Plugin{
     public function init(){
         $this->api->addHandler("player.block.touch", array($this, "preventBreakPlace"), 15); 
         //Getting ready for the player.block.touch -Leon
+        
         $this->api->addHandler("player.death", array($this, "BanPlayer"), 15); //Banning is awesome -Junyi00
+        
         $this->api->console->register("cash","Having to deal with SAO-MCPE Economy",array($this, "Economy"));
         $this->api->ban->cmdWhitelist("cash");
+        
         $this->path = $this->api->plugin->configPath($this);
+        
         $this->cash = new Config($this->path . "Economy.yml", CONFIG_YAML, for($i = 1, $i < 1,000,000,000, $i++){
             $i, "User" => "", "Money", "";//this config should do a for loop for each new member that joins -Glitch
         }) //don't understand how the loop will work O.o
         $this->cash = $this->api->plugin->readYAML($this->path . "Economy.yml");//Makes it read YAML :P
+        
         $this->DetectSkill = new Config($this->path . "DetectionSkill.yml", CONFIG_YAML, for($i = 1, $i < 1,000,000,000, $i++){
             $i, "User" => "", "SkillLevel", "";//this config should do a for loop for each new member that joins -Glitch
         })
         $this->DetectSkill = $this->api->plugin->readYAML($this->path . "DetectionSkill.yml")
+        
+        $this->api->schedule(20* 20, array($this, "Healing"), array(), false); //20 secs to heal 1 heatlh
     }
     //Shouldn't we be using storing data using SQL? There are othe stuffs to store too, afraid sing so much yaml would lag the server-Junyi00
 
@@ -58,5 +65,15 @@ class SAOMCPE implements Plugin{
         $username = $issuer->username;
         $money = $this->cash["Money"];//this is incomplete, I will keep working on it in a bit -Glitch
     }
+    
+    public function Healing() {
+        $players = $this->api->player->online();
+        for($i=1;$i<count($players);$i++) {
+            $player = $this->api->player->get($players[$i]);
+            if ($player->getHealth() != 20) { 
+                $player->setHealth($player->getHealth()+1, "Healing"); //heal 1 health
+            }
+        }
+    } //Done? Someone test for me plz :P -Junyi00
     
 }
